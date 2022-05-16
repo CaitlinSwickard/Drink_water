9d13fcec1127e7359c5e1c5fefea4227c4bc9af0
Caitlin Swickard <caitlin6029@gmail.com>
2022-05-16 17:33:32 -0600

    created js function to fill in big cup as small cups get clicked

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const liters = document.getElementById('liters');
 const percentage = document.getElementById('percentage');
 const remained = document.getElementById('remained');
 
+updateBigCup();
+
 // looping through cups to get selected clicked small cup to fill
 smallCups.forEach((cup, idx) => {
   // console.log(idx);
@@ -18,8 +20,8 @@ function highlightCups(idx) {
     idx--;
   }
 
-// looping thorough the small cups to add the full class list or remove as needed. 
-// The 'if' check will allow for someone to click on the 4th cup but also fill in the cups before it. 
+  // looping thorough the small cups to add the full class list or remove as needed. 
+  // The 'if' check will allow for someone to click on the 4th cup but also fill in the cups before it. 
   smallCups.forEach((cup, innerIdx) => {
     if (innerIdx <= idx) {
       cup.classList.add('full')
@@ -27,4 +29,33 @@ function highlightCups(idx) {
       cup.classList.remove('full')
     }
   })
+
+  updateBigCup();
+}
+
+// function to fill in the big cup with the amount of small cups that are highlighted
+function updateBigCup() {
+  // gets the amount of full cups
+  const fullCups = document.querySelectorAll('.cup-small.full').length;
+  // get total cups
+  const totalCups = smallCups.length;
+
+  // if cups are empty get rid of big cup text, or add it if cups are filled
+  if (fullCups === 0) {
+    percentage.style.visibility = 'hidden';
+    percentage.style.height = 0;
+  } else {
+    percentage.style.visibility = 'visible';
+    // get height by taking the fullcups / totalcups * the size of the cup
+    percentage.style.height = `${fullCups / totalCups * 330}px`;
+    percentage.innerText = `${fullCups / totalCups * 100}%`;
+  }
+
+  if (fullCups === totalCups) {
+    remained.style.visibility = 'hidden';
+    remained.style.height = 0;
+  } else {
+    remained.style.visibility = 'visible';
+    liters.innerText = `${2 - (250 * fullCups / 1000)}`
+  }
 }
